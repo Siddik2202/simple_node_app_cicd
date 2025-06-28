dd7b3f58d7ac1d34395c5e9d25374e3cbd641375
Abu Bakkar Siddik <136995650+Siddik2202@users.noreply.github.com>
2025-06-28 20:05:01 +0530

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,3 +1,15 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS sampledb;
+
+-- Allow access to user 'siddik' from any host (important for Docker)
+CREATE USER IF NOT EXISTS 'siddik'@'%' IDENTIFIED BY 'siddik';
+GRANT ALL PRIVILEGES ON sampledb.* TO 'siddik'@'%';
+FLUSH PRIVILEGES;
+
+-- Use the database
+USE sampledb;
+
+-- Create the nodeuser table if it doesn't exist
 CREATE TABLE IF NOT EXISTS nodeuser (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100),
